9f763628f0eb2dfee43912c3112bc3c171eff009
Yunus Altay <yunusaltay.nl@gmail.com>
2023-09-22 14:47:58 +0200

    new secret&configmap files added

--- Secret_configmap/commands.sh
+++ Secret_configmap/commands.sh
@@ -1,3 +1,4 @@
+##### mypod.yaml #####
 # create a configmap named 'myconfig' with values mycolor=blue
 kubectl create cm myconfig --from-literal=mycolor=blue
 kubectl get cm
@@ -7,9 +8,29 @@ kubectl apply -f mypod.yam
 kubectl exec -it clarusweb-pod -- sh
 echo $COLOR
 # blue
+
+##### mysecondpod.yaml #####
 # create a secret
 echo -n "secret123" > password.txt
 kubectl create secret generic mysecret --from-file=password=./password.txt
 kubectl get secret
 kubectl get secret mysecret -o yaml
 kubectl apply -f mysecondpod.yaml
+
+##### mythirdpod.yaml #####
+# Encode the secrets using base64 
+echo -n 'admin' | base64
+# YWRtaW4=
+echo -n '1f2d1e2e67df' | base64
+# MWYyZDFlMmU2N2Rm
+kubectl apply -f mythirdpod.yaml
+
+##### mydeployment.yaml #####
+# use configmaps as files from a pod
+kubectl create cm myconfig --from-literal=game=DOTA --from-literal=type=MOBA --from-literal=company=steam --from-literal=platform=steam
+kubectl get cm
+kubectl get cm myconfig -o yaml
+kubectl apply -f mydeployment.yaml
+kubectl exec -it <pod-name> --sh
+cd /usr/share/nginx/html
+ls
